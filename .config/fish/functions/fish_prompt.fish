92f0160026c86902b7582623e3435943d16bfe4a
Adam Houston <phazyy9@gmail.com>
2016-03-04 18:05:54 +0000

    updating Atom and terminal configs

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,15 @@
 function fish_prompt
-    set_color red
-    echo -n '┌─╼'
+    set_color purple
+    echo -n '┌─╼ '
     set_color white
-    echo -n '[' (whoami) (pwd) ']'
-    set_color red
+    echo -n (whoami)
+
+    echo -n ' ['
+    set_color blue
+    echo -n (prompt_pwd)
+    set_color white
+    echo -n ']'
+
+    set_color purple
     echo -e '\n└────╼ '
 end
