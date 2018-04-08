e724c5da39f8ce612f6714ef2d14798a2f042f67
Adam Houston <phazyy9@gmail.com>
2018-04-08 13:31:45 +0100

    atom: removing unused config
    Former-commit-id: d3195e5db125afe6d653d5427703e766b8358d8a

--- atom/.atom/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# title       : setup
-# description : setup atom-editor and install packages
-# author      : Adam Houston (@phazyy)
-# usage       : sh ./setup.sh
-
-cp config.cson ~/.atom/
-cp keymap.cson ~/.atom/
-apm install color-picker easy-motion-redux emmet ex-mode language-javascript-jsx linter linter-jshint pigments react vim-mode
