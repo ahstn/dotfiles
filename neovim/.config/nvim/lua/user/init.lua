51562ff2853ef7bd2509dc10b92961be636720a0
Adam Houston <phazyy9@gmail.com>
2023-06-02 10:52:49 +0100

    nvim: trying astronvim

---
+++ neovim/.config/nvim/lua/user/init.lua
@@ -0,0 +1,69 @@
+return {
+  -- Configure AstroNvim updates
+  updater = {
+    remote = "origin", -- remote to use
+    channel = "stable", -- "stable" or "nightly"
+    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly", -- branch name (NIGHTLY ONLY)
+    skip_prompts = false, -- skip prompts about breaking changes
+    show_changelog = true, -- show the changelog after performing an update
+  },
+
+  -- Set colorscheme to use
+  colorscheme = "astrodark",
+  diagnostics = {
+    virtual_text = true,
+    underline = true,
+  },
+  lsp = {
+    formatting = {
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        ignore_filetypes = { 
+          "markdown"
+        },
+      },
+      timeout_ms = 1000, -- default format timeout
+    },
+    -- enable servers that you already have installed without mason
+    servers = {
+      -- "pyright"
+    },
+  },
+
+  -- Configure require("lazy").setup() options
+  lazy = {
+    defaults = { lazy = true },
+    performance = {
+      rtp = {
+        -- customize default disabled vim plugins
+        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
+      },
+    },
+  },
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.colorscheme.catppuccin" }
+    -- ... import any community contributed plugins here
+  }, 
+
+
+  -- This function is run last and is a good place to configuring
+  -- augroups/autocommands and custom filetypes also this just pure lua so
+  -- anything that doesn't fit in the normal config locations above can go here
+  polish = function()
+    -- Set up custom filetypes
+    -- vim.filetype.add {
+    --   extension = {
+    --     foo = "fooscript",
+    --   },
+    --   filename = {
+    --     ["Foofile"] = "fooscript",
+    --   },
+    --   pattern = {
+    --     ["~/%.config/foo/.*"] = "fooscript",
+    --   },
+    -- }
+  end,
+}
